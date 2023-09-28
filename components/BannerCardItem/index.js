745dbb3fd87df77104c8f6cb64fc318096971cf4
akdixit-951 <akdixit951@gmail.com>
2023-09-28 14:34:57 +0000

    Banners

---
+++ components/BannerCardItem/index.js
@@ -0,0 +1,19 @@
+import './index.css'
+
+const BannerCardItem = props => {
+  const {sourceItem} = props
+  const {headerText, description, className} = sourceItem
+  return (
+    <li className={className}>
+      <div>
+        <h1 className="card-heading">{headerText}</h1>
+        <p className="card-desc">{description}</p>
+        <button type="button" className="card-btn">
+          Show More
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default BannerCardItem
